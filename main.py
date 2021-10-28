90b5776b3d82c2046a28274a2fedce73ca38df72
Шакмаев Александр <savproga@gmail.com>
2021-10-28 22:09:54 +0300

    add docstring

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ post_list = []
 
 
 def get_posts(url, max_depth=0):
+    """max_depth - определяет глубину рекурсии"""
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/94.0.4606.81 Safari/537.36'}
     response = requests.get(url, headers=headers)
@@ -29,7 +30,7 @@ def write_to_file(line):
         file.write(line + "\n")
 
 
-get_posts('https://stackoverflow.com/questions', 1)
+get_posts('https://stackoverflow.com/questions', 0)
 
 args = sys.argv
 print_in_file = False
